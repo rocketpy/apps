cbfe75e877ffa3002000cb90329218e895e9c024
rocketpy <dimitr.ushakov@gmail.com>
2023-01-29 23:56:46 +0200

    Update pynecone_app.py

--- Pynecone/pynecone_app.py
+++ Pynecone/pynecone_app.py
@@ -1,1 +1,35 @@
+# 
 
+# Component: https://pynecone.io/docs/library
+
+#  Example
+
+import pynecone as pc
+
+
+class State(pc.State):
+    count: int = 0
+
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
+
+
+def index():
+    return pc.hstack(
+        pc.button(
+            "Decrement",
+            color_scheme="red",
+            border_radius="1em",
+            on_click=State.decrement,
+        ),
+        pc.heading(State.count, font_size="2em"),
+        pc.button(
+            "Increment",
+            color_scheme="green",
+            border_radius="1em",
+            on_click=State.increment,
+        ),
+    )
