dc1ad23cff934f54c7206214683a49593e5c8d85
rocketpy <dimitr.ushakov@gmail.com>
2023-01-31 20:27:18 +0200

    Update pynecone_app.py

--- Pynecone/pynecone_app.py
+++ Pynecone/pynecone_app.py
@@ -1,4 +1,5 @@
-# Build web apps in minutes. Deploy with a single command.
+# Web apps in pure Python - Build web apps in minutes. Deploy with a single command.
+
 
 # Component: https://pynecone.io/docs/library
 
@@ -8,8 +9,33 @@
 # cd my_app_name
 # pc init
 
-#  Example
 
+# Example Pynecone App
+import pynecone as pc
+import openai
+
+openai.api_key = "YOUR_API_KEY"
+
+class State(pc.State):
+    """The app state."""
+    prompt = ""
+    image_url = ""
+    image_processing = False
+    image_made = False
+
+    def process_image(self):
+        """Set the image processing flag to true and indicate image is not made yet."""
+        self.image_processing = True
+        self.image_made = False        
+
+    def get_image(self):
+        """Get the image from the prompt."""
+        response = openai.Image.create(prompt=self.prompt, n=1, size="1024x1024")
+        self.image_url = response["data"][0]["url"]
+        self.image_processing = False
+        self.image_made = True
+
+#  Example
 import pynecone as pc
 
 
