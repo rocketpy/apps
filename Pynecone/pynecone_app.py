a4c62c583651a4828c467de4c0a631ebb0dece76
rocketpy <dimitr.ushakov@gmail.com>
2023-01-31 20:28:10 +0200

    Update pynecone_app.py

--- Pynecone/pynecone_app.py
+++ Pynecone/pynecone_app.py
@@ -34,6 +34,45 @@ class State(pc.State):
         self.image_url = response["data"][0]["url"]
         self.image_processing = False
         self.image_made = True
+        
+def index():
+    return pc.center(
+        pc.vstack(
+            pc.heading("DALL·E", font_size="1.5em"),
+            pc.input(placeholder="Enter a prompt..", on_blur=State.set_prompt),
+            pc.button(
+                "Generate Image",
+                on_click=[State.process_image, State.get_image],
+                width="100%",
+            ),
+            pc.divider(),
+            pc.cond(
+                State.image_processing,
+                pc.circular_progress(is_indeterminate=True),
+                pc.cond(
+                     State.image_made,
+                     pc.image(
+                         src=State.image_url,
+                         height="25em",
+                         width="25em",
+                    )
+                )
+            ),
+            bg="white",
+            padding="2em",
+            shadow="lg",
+            border_radius="lg",
+        ),
+        width="100%",
+        height="100vh",
+        bg="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%)",
+    )
+
+# Add state and page to the app.
+app = pc.App(state=State)
+app.add_page(index, title="Pynecone:DALL·E")
+app.compile()
+
 
 #  Example
 import pynecone as pc
